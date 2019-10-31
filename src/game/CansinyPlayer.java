11e6c2b77c4b74d6f5e3419e1b11b3676a2c594f
Cansiny0320 <51797219+Cansiny0320@users.noreply.github.com>
2019-10-31 10:49:48 +0800

    第3次-level2-王炬欣-2019211590
    first commit

---
+++ src/game/CansinyPlayer.java
@@ -0,0 +1,77 @@
+package game.player;
+
+import game.Manager;
+import game.Player;
+import game.Poker;
+
+import java.util.Collections;
+import java.util.List;
+
+public class CansinyPlayer implements Player {
+    @Override
+    public String getName() {
+
+
+        return "王炬欣";
+    }
+
+    @Override
+    public String getStuNum() {
+        return "2019211590";
+    }
+
+    @Override
+    public void onGameStart(Manager manager, int totalPlayer) {
+    }
+
+
+    @Override
+    public int bet(int time, int round, int lastPerson, int moneyOnDesk, int moneyYouNeedToPayLeast, List<Poker> pokers) {
+        if (isSameColor(pokers) || isSameColorStraight(pokers) || isSamePoint(pokers))
+            return 3 * moneyYouNeedToPayLeast;
+        if(isStraight(pokers))
+            return (int)((2.3 * moneyYouNeedToPayLeast));
+        if(isPair(pokers) && time>=20)
+            return (int) (1.3 * moneyYouNeedToPayLeast) < 3 * moneyOnDesk ? (int) (1.3 * moneyYouNeedToPayLeast) : moneyYouNeedToPayLeast;
+        if(moneyYouNeedToPayLeast == 100   && lastPerson == 2)
+            return moneyYouNeedToPayLeast;
+        return 0;
+    }
+
+    @Override
+    public void onResult(int time, boolean isWin, List<Poker> pokers) {
+
+    }
+
+    private boolean isSameColor(List<Poker> pokers) {
+        return pokers.get(0).getSuit() == pokers.get(1).getSuit() &&
+                pokers.get(1).getSuit() == pokers.get(2).getSuit();
+    }
+
+    private boolean isPair(List<Poker> pokers) {
+        return pokers.get(0).getPoint().getNum() == pokers.get(1).getPoint().getNum()
+                || pokers.get(1).getPoint().getNum() == pokers.get(2).getPoint().getNum()
+                || pokers.get(0).getPoint().getNum() == pokers.get(2).getPoint().getNum();
+    }
+
+    private boolean isSameColorStraight(List<Poker> handCards) {
+        return isSameColor(handCards) && isStraight(handCards);
+    }
+
+    private boolean isSamePoint(List<Poker> handCards) {
+        return handCards.get(0).getPoint() == handCards.get(1).getPoint()
+                && handCards.get(2).getPoint() == handCards.get(1).getPoint();
+    }
+
+    private boolean isStraight(List<Poker> pokers) {
+        Collections.sort(pokers);
+        return Math.abs(pokers.get(0).getPoint().getNum() - pokers.get(1).getPoint().getNum()) == 1
+                && Math.abs(pokers.get(1).getPoint().getNum() - pokers.get(2).getPoint().getNum()) == 1;
+
+    }
+
+    private boolean trick(){
+
+        return true;
+    }
+}
