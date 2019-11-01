e282406799ab6dff9287bec761312c72bb3aaea6
Galaxy955 <2659029033@qq.com>
2019-11-01 21:39:45 +0800

    更新

--- src/game/player/LiuPlayer.java
+++ src/game/player/LiuPlayer.java
@@ -8,12 +8,12 @@ import java.util.List;
 
 public class LiuPlayer implements Player {
     public String getName() {
-        return "一刀刘";
+        return "刘远熹";
     }
 
     @Override
     public String getStuNum() {
-        return "2019213521";
+        return "2019213186";
     }
 
     @Override
